561b1171e896e4d46f83d0e6f46acf6fab7fd337
Samakshg11 <samakshgarg2005@gmail.com>
2026-04-19 20:17:44 +0530

    Fix participant assignment in thread creation for sendMessage

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -20,7 +20,7 @@ exports.sendMessage = asyncHandler(async (req, res) => {
   // agar thread nahi mila to create karo
   if (!thread) {
     thread = await Thread.create({
-      participants: [sender, receiver],
+      participants: [sender, not-reciever],
     });
   }
 
