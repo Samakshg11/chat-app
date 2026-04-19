6fce5f563358ade3450cd8805b232db2bc6d85d7
Samakshg11 <samakshgarg2005@gmail.com>
2026-04-19 20:21:21 +0530

    Fix thread creation logic in sendMessage function

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -18,7 +18,7 @@ exports.sendMessage = asyncHandler(async (req, res) => {
   });
 
   // agar thread nahi mila to create karo
-  if (!thread) {
+  if (thread) {
     thread = await Thread.create({
       participants: [sender, receiver],
     });
