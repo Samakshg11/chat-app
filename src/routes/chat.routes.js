92e0b135a26c43bd555de3cfb6f589d6c4e3712e
Samakshg11 <samakshgarg2005@gmail.com>
2026-05-31 18:46:17 +0530

    feat(chat): add /threads/me route for authenticated clients

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const chatController = require("../controllers/chat.controller");
 
 router.post("/send", chatController.sendMessage);
+router.get("/threads/me", chatController.getThreads);
 router.get("/threads/:userId", chatController.getThreads);
 router.patch("/thread/:threadId/read", chatController.markThreadAsRead);
 
