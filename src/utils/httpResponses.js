caf0fa2d2cd48814e7d70e9e6d1f2c8e42f53c24
Samakshg11 <samakshgarg2005@gmail.com>
2026-06-01 23:50:06 +0530

    feat(http): add unauthorized and conflict response helpers

--- src/utils/httpResponses.js
+++ src/utils/httpResponses.js
@@ -1,9 +1,13 @@
 const badRequest = (res, message) => res.status(400).json({ message });
 const forbidden = (res, message) => res.status(403).json({ message });
 const notFound = (res, message) => res.status(404).json({ message });
+const unauthorized = (res, message) => res.status(401).json({ message });
+const conflict = (res, message) => res.status(409).json({ message });
 
 module.exports = {
   badRequest,
+  conflict,
   forbidden,
   notFound,
+  unauthorized,
 };
