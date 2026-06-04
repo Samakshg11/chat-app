276ea257b1327edf02cb5325ebfac4f12cbb537a
Samakshg11 <samakshgarg2005@gmail.com>
2026-06-04 23:53:06 +0530

    Document chat constants with brief comments

--- src/config/chat.constants.js
+++ src/config/chat.constants.js
@@ -1,3 +1,10 @@
+/**
+ * Chat-related defaults and limits.
+ * - DEFAULT_LIMIT: default messages per page
+ * - DEFAULT_PAGE: default pagination start
+ * - MAX_LIMIT: maximum allowed page size for safety
+ * - MAX_MESSAGE_LENGTH: maximum characters allowed in a message
+ */
 module.exports = {
   DEFAULT_LIMIT: 20,
   DEFAULT_PAGE: 1,
