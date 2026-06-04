aac705791fa0954c1e3e44c2b6eae1643f91cea8
Samakshg11 <samakshgarg2005@gmail.com>
2026-06-04 23:52:43 +0530

    Log app environment at startup in src/app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,15 @@ const path = require("path");
 
 const app = express();
 
+// Set environment
+app.set("env", process.env.NODE_ENV || "development");
+
 app.use(express.json());
 
 // serve frontend
 app.use(express.static(path.join(__dirname, "../public")));
+
+// Helpful startup log when required by server
+console.info(`[app] environment: ${app.get("env")}`);
 
 module.exports = app;
