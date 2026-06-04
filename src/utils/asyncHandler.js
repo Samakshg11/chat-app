103b7e54b3f12fccbdaec8512aa968411d9f5397
Samakshg11 <samakshgarg2005@gmail.com>
2026-06-04 23:53:54 +0530

    Add JSDoc to asyncHandler utility

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,2 +1,10 @@
+/**
+ * Helper to wrap async route handlers and forward errors to express error handler.
+ *
+ * Usage: `app.get('/path', asyncHandler(async (req, res) => { ... }))`
+ *
+ * @param {Function} fn Async route handler (req, res, next)
+ * @returns {Function} Express middleware function
+ */
 module.exports = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
